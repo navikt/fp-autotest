93569157c4c5409274d2b51ed05bcc70300db121
christofferlohne <45653861+christofferlohne@users.noreply.github.com>
2019-06-25 12:30:43 +0200

    Pfp 8404 svp test (#2)
    * PFP-8404: Logger inn fordeling.
    
    * PFP-8404 Legger til IM for SVP søknad og oppdaterer versjon for dokumentgenerator i POM
    
    * PFP-8404 legger til nytt testscenario for SVP, IM før søknad. Feiler på tilkjentytelse
    
    * PFP-8404 Oppdaterer POM med nyere versjon av VTP, disabler SVP test som ikke er verifisert.

--- src/test/java/no/nav/foreldrepenger/autotest/foreldrepenger/svangerskapspenger/Førstegangsbehandling.java
+++ src/test/java/no/nav/foreldrepenger/autotest/foreldrepenger/svangerskapspenger/Førstegangsbehandling.java
@@ -1,10 +1,12 @@
 package no.nav.foreldrepenger.autotest.foreldrepenger.svangerskapspenger;
 
 import static no.nav.foreldrepenger.fpmock2.dokumentgenerator.foreldrepengesoknad.erketyper.SoekerErketyper.morSoeker;
+import static no.nav.foreldrepenger.fpmock2.dokumentgenerator.inntektsmelding.erketyper.InntektsmeldingBuilder.createDefaultSvangerskapspenger;
 
 import java.time.LocalDate;
 import java.util.Arrays;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
@@ -12,10 +14,10 @@ import no.nav.foreldrepenger.autotest.aktoerer.Aktoer;
 import no.nav.foreldrepenger.autotest.base.SvangerskapspengerTestBase;
 import no.nav.foreldrepenger.fpmock2.dokumentgenerator.foreldrepengesoknad.erketyper.ArbeidsforholdErketyper;
 import no.nav.foreldrepenger.fpmock2.dokumentgenerator.foreldrepengesoknad.erketyper.MedlemskapErketyper;
-import no.nav.foreldrepenger.fpmock2.dokumentgenerator.foreldrepengesoknad.erketyper.OpptjeningErketyper;
 import no.nav.foreldrepenger.fpmock2.dokumentgenerator.foreldrepengesoknad.erketyper.SvangerskapspengerYtelseErketyper;
 import no.nav.foreldrepenger.fpmock2.dokumentgenerator.foreldrepengesoknad.erketyper.TilretteleggingsErketyper;
 import no.nav.foreldrepenger.fpmock2.dokumentgenerator.foreldrepengesoknad.soeknad.ForeldrepengesoknadBuilder;
+import no.nav.foreldrepenger.fpmock2.dokumentgenerator.inntektsmelding.erketyper.InntektsmeldingBuilder;
 import no.nav.foreldrepenger.fpmock2.kontrakter.TestscenarioDto;
 import no.nav.foreldrepenger.fpmock2.testmodell.dokument.modell.koder.DokumenttypeId;
 import no.nav.vedtak.felles.xml.soeknad.svangerskapspenger.v1.Virksomhet;
@@ -27,35 +29,61 @@ public class Førstegangsbehandling extends SvangerskapspengerTestBase {
     @Test
     public void morSøkerSvangerskapspengerHelTilretteleggingFireUkerFørTermin() throws Exception {
         TestscenarioDto testscenario = opprettScenario("50");
+
         String morAktoerId = testscenario.getPersonopplysninger().getSøkerAktørIdent();
         String fnrMor = testscenario.getPersonopplysninger().getSøkerIdent();
+        int beløpMor = testscenario.getScenariodata().getInntektskomponentModell().getInntektsperioder().get(0).getBeløp();
         String orgNrMor = testscenario.getScenariodata().getArbeidsforholdModell().getArbeidsforhold().get(0).getArbeidsgiverOrgnr();
         Virksomhet morVirksomhet = ArbeidsforholdErketyper.virksomhet(orgNrMor);
-
         ForeldrepengesoknadBuilder soknad = ForeldrepengesoknadBuilder.startBuilding()
                 .withSvangerskapspengeYtelse(
-                        SvangerskapspengerYtelseErketyper.svangerskapspengerMedOpptjening(
+                        SvangerskapspengerYtelseErketyper.svangerskapspenger(
                                 LocalDate.now().plusWeeks(4),
                                 MedlemskapErketyper.medlemskapNorge(),
-                                OpptjeningErketyper.medEgenNaeringOpptjening(),
                                 Arrays.asList(TilretteleggingsErketyper.helTilrettelegging(LocalDate.now(),LocalDate.now().plusWeeks(1),morVirksomhet))
-                        ))
+                        )
+                )
                 .withSoeker(morSoeker(morAktoerId))
-                ;
-        soknad.build();
-
+                .withMottattDato(LocalDate.now());
+        fordel.erLoggetInnMedRolle(Aktoer.Rolle.SAKSBEHANDLER);
         long saksnummer = fordel.sendInnSøknad(soknad.build(), testscenario, DokumenttypeId.SØKNAD_SVANGERSKAPSPENGER);
 
         // Inntektsmelding
-        // XSD feiler på svangerskapspenger.
+        InntektsmeldingBuilder inntektsmeldingerSøker = createDefaultSvangerskapspenger(beløpMor, fnrMor, orgNrMor);
+        fordel.sendInnInntektsmelding(inntektsmeldingerSøker, testscenario, saksnummer);
 
-
-        saksbehandler.erLoggetInnMedRolle(Aktoer.Rolle.SAKSBEHANDLER);
         saksbehandler.hentFagsak(saksnummer);
+    }
+    @Test
+    @Disabled
+    public void mor_SVP_imFørSøknad() throws Exception {
+        //Feiler på tilkjentytelse.
+        TestscenarioDto testscenario = opprettScenario("50");
+
+        String morAktoerId = testscenario.getPersonopplysninger().getSøkerAktørIdent();
+        String fnrMor = testscenario.getPersonopplysninger().getSøkerIdent();
+        int beløpMor = testscenario.getScenariodata().getInntektskomponentModell().getInntektsperioder().get(0).getBeløp();
+        String orgNrMor = testscenario.getScenariodata().getArbeidsforholdModell().getArbeidsforhold().get(0).getArbeidsgiverOrgnr();
+        Virksomhet morVirksomhet = ArbeidsforholdErketyper.virksomhet(orgNrMor);
+
+        fordel.erLoggetInnMedRolle(Aktoer.Rolle.SAKSBEHANDLER);
+        InntektsmeldingBuilder inntektsmeldingerSøker = createDefaultSvangerskapspenger(beløpMor, fnrMor, orgNrMor);
+        Long saksnummer = fordel.sendInnInntektsmelding(inntektsmeldingerSøker, testscenario, null);
 
 
+        ForeldrepengesoknadBuilder soknad = ForeldrepengesoknadBuilder.startBuilding()
+                .withSvangerskapspengeYtelse(
+                        SvangerskapspengerYtelseErketyper.svangerskapspenger(
+                                LocalDate.now().plusWeeks(4),
+                                MedlemskapErketyper.medlemskapNorge(),
+                                Arrays.asList(TilretteleggingsErketyper.helTilrettelegging(LocalDate.now(),LocalDate.now().plusWeeks(1),morVirksomhet))
+                        )
+                )
+                .withSoeker(morSoeker(morAktoerId))
+                .withMottattDato(LocalDate.now());
+        fordel.sendInnSøknad(soknad.build(), testscenario, DokumenttypeId.SØKNAD_SVANGERSKAPSPENGER, saksnummer);
 
-
+        saksbehandler.hentFagsak(saksnummer);
     }
 
 
