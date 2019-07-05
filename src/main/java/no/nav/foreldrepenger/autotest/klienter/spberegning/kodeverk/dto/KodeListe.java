59cfd14025735b814c6c347af6aec04f40d38cab
Christoffer <christoffer.lohne@soprasteria.com>
2019-07-05 13:36:04 +0200

    Legger til SVP objekter for tilrettelegging av informasjon om SVP fra VL for autotest

--- src/main/java/no/nav/foreldrepenger/autotest/klienter/spberegning/kodeverk/dto/KodeListe.java
+++ src/main/java/no/nav/foreldrepenger/autotest/klienter/spberegning/kodeverk/dto/KodeListe.java
@@ -1,11 +1,23 @@
 package no.nav.foreldrepenger.autotest.klienter.spberegning.kodeverk.dto;
 
 import java.util.ArrayList;
+import java.util.Objects;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class KodeListe extends ArrayList<Kode>{
+    private String kode;
+    private String kodeverk;
+
+    public KodeListe (){
+    }
+    public KodeListe(String kode, String kodeverk){
+        Objects.requireNonNull(kode, "kode"); //$NON-NLS-1$
+        Objects.requireNonNull(kodeverk, "kodeverk"); //$NON-NLS-1$
+        this.kode = kode;
+        this.kodeverk = kodeverk;
+    }
     
     /** @deprecated - IKKE bruk navn for kodeverdi ved oppslag, bruk kode. */
     @Deprecated
