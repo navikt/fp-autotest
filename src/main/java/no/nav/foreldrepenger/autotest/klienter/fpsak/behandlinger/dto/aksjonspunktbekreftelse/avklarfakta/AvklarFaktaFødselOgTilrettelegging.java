5af2b62c8a6f8e35978db0aca816069befcc8628
olovholm <ola.lovholm@nav.no>
2019-07-08 16:17:04 +0200

    NOJIRA: Støtte aksjonspunkt 5091

--- src/main/java/no/nav/foreldrepenger/autotest/klienter/fpsak/behandlinger/dto/aksjonspunktbekreftelse/avklarfakta/AvklarFaktaFødselOgTilrettelegging.java
+++ src/main/java/no/nav/foreldrepenger/autotest/klienter/fpsak/behandlinger/dto/aksjonspunktbekreftelse/avklarfakta/AvklarFaktaFødselOgTilrettelegging.java
@@ -2,27 +2,31 @@ package no.nav.foreldrepenger.autotest.klienter.fpsak.behandlinger.dto.aksjonspu
 
 import no.nav.foreldrepenger.autotest.klienter.fpsak.behandlinger.dto.aksjonspunktbekreftelse.AksjonspunktBekreftelse;
 import no.nav.foreldrepenger.autotest.klienter.fpsak.behandlinger.dto.aksjonspunktbekreftelse.BekreftelseKode;
-import no.nav.foreldrepenger.autotest.klienter.fpsak.behandlinger.dto.aksjonspunktbekreftelse.SvpArbeidsforhold;
 import no.nav.foreldrepenger.autotest.klienter.fpsak.behandlinger.dto.behandling.Behandling;
-import no.nav.foreldrepenger.autotest.klienter.fpsak.behandlinger.dto.behandling.arbeid.Arbeidsforhold;
+import no.nav.foreldrepenger.autotest.klienter.fpsak.behandlinger.dto.behandling.svangerskapspenger.Arbeidsforhold;
 import no.nav.foreldrepenger.autotest.klienter.fpsak.fagsak.dto.Fagsak;
-import no.nav.foreldrepenger.fpmock2.dokumentgenerator.foreldrepengesoknad.erketyper.TilretteleggingsErketyper;
 
-import java.math.BigDecimal;
 import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.util.ArrayList;
 import java.util.List;
 
 @BekreftelseKode(kode="5091")
 public class AvklarFaktaFødselOgTilrettelegging extends AksjonspunktBekreftelse {
-    private LocalDate termindato;
-    private LocalDate fødselsdato;
-    private List<SvpArbeidsforhold> bekreftetSvpArbeidsforholdList;
+    protected LocalDate termindato;
+    protected LocalDate fødselsdato;
+    protected List<Arbeidsforhold> bekreftetSvpArbeidsforholdList;
+    protected List<Arbeidsforhold> arbeidsforhold;
 
 
     public AvklarFaktaFødselOgTilrettelegging(Fagsak fagsak, Behandling behandling){
         super(fagsak, behandling);
+
+        this.termindato = behandling.getTilrettelegging().getTermindato();
+        this.fødselsdato = behandling.getTilrettelegging().getFødselsdato();
+        this.bekreftetSvpArbeidsforholdList = behandling.getTilrettelegging().getArbeidsforholdList();
+        this.arbeidsforhold = behandling.getTilrettelegging().getArbeidsforholdList();
+
+
+
     }
 
 }
