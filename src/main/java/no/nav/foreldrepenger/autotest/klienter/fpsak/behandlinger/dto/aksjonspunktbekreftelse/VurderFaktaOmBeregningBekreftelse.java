64e73e1a0fb5b8b6ed76ec3371d43eb29c7be215
Espen Velsvik <espen.velsvik@nav.no>
2019-08-01 12:50:34 +0200

    NOJIRA Fikse feilende tester grunnet endring i Dto fra frontend i beregning

--- src/main/java/no/nav/foreldrepenger/autotest/klienter/fpsak/behandlinger/dto/aksjonspunktbekreftelse/VurderFaktaOmBeregningBekreftelse.java
+++ src/main/java/no/nav/foreldrepenger/autotest/klienter/fpsak/behandlinger/dto/aksjonspunktbekreftelse/VurderFaktaOmBeregningBekreftelse.java
@@ -13,96 +13,47 @@ import no.nav.foreldrepenger.autotest.klienter.fpsak.kodeverk.dto.Kode;
 @BekreftelseKode(kode="5058")
 public class VurderFaktaOmBeregningBekreftelse extends AksjonspunktBekreftelse {
 
-    protected FastsettMaanedsinntektFL fastsettMaanedsinntektFL;
-    protected FastsettMaanedsinntektUtenInntektsmelding fastsattUtenInntektsmelding;
-    protected List<String> faktaOmBeregningTilfeller = new ArrayList<>();
-    protected MottarYtelse mottarYtelse;
-    protected FastsettEndretBeregningsgrunnlag fastsettEndringBeregningsgrunnlag;
-    protected VurderTidsbegrensetArbeidsforholdDto vurderTidsbegrensetArbeidsforhold;
-    protected YtelseForedeling kunYtelseFordeling;
+    protected FaktaOmBeregningLagreDto fakta = new FaktaOmBeregningLagreDto();
 
     public VurderFaktaOmBeregningBekreftelse(Fagsak fagsak, Behandling behandling) {
         super(fagsak, behandling);
     }
 
     public VurderFaktaOmBeregningBekreftelse leggTilFaktaOmBeregningTilfeller(String kode) {
-        this.faktaOmBeregningTilfeller.add(kode);
+        fakta.leggTilFaktaOmBeregningTilfeller(kode);
         return this;
     }
 
     public VurderFaktaOmBeregningBekreftelse leggTilVurderTidsbegrenset(List<VurderteArbeidsforholdDto> tidsbegrensetAndeler){
-        this.vurderTidsbegrensetArbeidsforhold = new VurderTidsbegrensetArbeidsforholdDto(tidsbegrensetAndeler);
+        fakta.leggTilVurderTidsbegrenset(tidsbegrensetAndeler);
         return this;
     }
 
     public VurderFaktaOmBeregningBekreftelse leggTilMottarYtelse(boolean mottarYtelse, List<ArbeidstakerandelUtenIMMottarYtelse> arbeidstakerandelUtenIMMottarYtelses){
-        this.mottarYtelse = new MottarYtelse(mottarYtelse, arbeidstakerandelUtenIMMottarYtelses);
+        fakta.leggTilMottarYtelse(mottarYtelse, arbeidstakerandelUtenIMMottarYtelses);
         return this;
     }
 
 
     public VurderFaktaOmBeregningBekreftelse leggTilMaanedsinntektUtenInntektsmelding(List<FastsettMaanedsinntektUtenInntektsmeldingAndel> andelListe){
-        this.fastsattUtenInntektsmelding = new FastsettMaanedsinntektUtenInntektsmelding(andelListe);
+        fakta.leggTilMaanedsinntektUtenInntektsmelding(andelListe);
         return this;
     }
 
     public VurderFaktaOmBeregningBekreftelse leggTilMaanedsinntekt(int maanedsinntekt) {
-        fastsettMaanedsinntektFL = new FastsettMaanedsinntektFL(maanedsinntekt);
+        fakta.leggTilMaanedsinntekt(maanedsinntekt);
         return this;
     }
 
     public VurderFaktaOmBeregningBekreftelse leggTilAndelerYtesle(double beløp, Kode inntektskategori) {
-        kunYtelseFordeling = new YtelseForedeling();
-        kunYtelseFordeling.leggTilYtelseAndeler(new YtelseAndeler(beløp, inntektskategori.kode));
+        fakta.leggTilAndelerYtesle(beløp, inntektskategori);
         return this;
     }
 
 
     public VurderFaktaOmBeregningBekreftelse leggTilAndelerEndretBg(BeregningsgrunnlagPeriodeDto periode, BeregningsgrunnlagPrStatusOgAndelDto andel, FastsatteVerdier fastsatteVerdier) {
-        if (fastsettEndringBeregningsgrunnlag == null) {
-            fastsettEndringBeregningsgrunnlag = new FastsettEndretBeregningsgrunnlag();
-        }
-        fastsettEndringBeregningsgrunnlag.leggTilAndelTilPeriode(periode, andel, fastsatteVerdier);
+        fakta.leggTilAndelerEndretBg(periode, andel, fastsatteVerdier);
         return this;
-    }
-
-
-    public class YtelseForedeling{
-
-        public List<YtelseAndeler> andeler = new ArrayList<>();
-
-        public YtelseForedeling() {
-            // TODO Auto-generated constructor stub
-        }
-
-        public void leggTilYtelseAndeler(YtelseAndeler andel) {
-            andel.setAndelsnr(andeler.size() + 1);
-            andeler.add(andel);
-        }
-
-    }
-
-    public class YtelseAndeler{
-        public int andelsnr;
-        public double fastsattBeløp;
-        public String inntektskategori;
-        public boolean lagtTilAvSaksbehandler;
-        public boolean nyAndel;
-
-        public YtelseAndeler() {
-            // TODO Auto-generated constructor stub
-        }
-
-        public YtelseAndeler(double fastsattBeløp, String inntektskategori) {
-            super();
-            this.fastsattBeløp = fastsattBeløp;
-            this.inntektskategori = inntektskategori;
-        }
-
-        public void setAndelsnr(int andelsnr) {
-            this.andelsnr = andelsnr;
-        }
-
     }
 
 }
