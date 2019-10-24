12d6e731fc4eb14aeb26a3446b32a120d70ef76b
Espen Waaga <espen.waaga@nav.no>
2019-10-24 12:40:06 +0200

    Oppdatert test i termin og inkludert ny metode i saksbehandler for å hente ut alle behandlinger på fagsak'

--- src/main/java/no/nav/foreldrepenger/autotest/base/TestScenarioTestBase.java
+++ src/main/java/no/nav/foreldrepenger/autotest/base/TestScenarioTestBase.java
@@ -2,19 +2,25 @@ package no.nav.foreldrepenger.autotest.base;
 
 import io.qameta.allure.Step;
 import no.nav.foreldrepenger.autotest.klienter.vtp.expect.ExpectKlient;
+import no.nav.foreldrepenger.autotest.klienter.vtp.testscenario.TestscenarioFraAutotestKlient;
 import no.nav.foreldrepenger.autotest.klienter.vtp.testscenario.TestscenarioKlient;
 import no.nav.foreldrepenger.autotest.util.http.BasicHttpSession;
+import no.nav.foreldrepenger.autotest.util.testscenario.TestscenarioReader;
 import no.nav.foreldrepenger.vtp.kontrakter.TestscenarioDto;
 
 import java.io.IOException;
 
 public abstract class TestScenarioTestBase extends TestBase {
-    
+
     protected TestscenarioKlient testscenarioKlient;
+    protected TestscenarioFraAutotestKlient testscenarioFraAutotestKlient;
+    protected TestscenarioReader testscenarioReader;
     protected ExpectKlient expectKlient;
-    
+
     public TestScenarioTestBase() {
         testscenarioKlient = new TestscenarioKlient(BasicHttpSession.session());
+        testscenarioFraAutotestKlient = new TestscenarioFraAutotestKlient(BasicHttpSession.session());
+        testscenarioReader = new TestscenarioReader();
         expectKlient = new ExpectKlient(BasicHttpSession.session());
     }
 
@@ -26,5 +32,11 @@ public abstract class TestScenarioTestBase extends TestBase {
     @Step("Oppretter testscenario {id}")
     protected TestscenarioDto opprettScenarioMedPrivatArbeidsgiver(String id, String aktorId) throws IOException {
         return testscenarioKlient.opprettTestscenarioMedAktorId(id, aktorId);
+    }
+
+    @Step("Oppretter testscenario {id} fra Json fil lokalisert i autotest")
+    protected TestscenarioDto initialiserScenario(String id) throws IOException {
+        Object testscenarioObject = testscenarioReader.LesOgReturnerScenarioFraJsonfil(id);
+        return (testscenarioObject == null ? null : testscenarioFraAutotestKlient.initialiserTestscenario(id, testscenarioObject));
     }
 }
