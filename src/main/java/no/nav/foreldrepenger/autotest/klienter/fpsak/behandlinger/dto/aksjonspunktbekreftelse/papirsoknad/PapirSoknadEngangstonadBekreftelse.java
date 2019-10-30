23c6c6c01cae56eaeb41ff25a38bd6d26a1e0f02
Christoffer Lohne <christoffer.lohne@soprasteria.com>
2019-10-30 15:15:05 +0100

    Legger til støtte for automatisk løse aksjonspunkt manuell registrering papirsøknad

--- src/main/java/no/nav/foreldrepenger/autotest/klienter/fpsak/behandlinger/dto/aksjonspunktbekreftelse/papirsoknad/PapirSoknadEngangstonadBekreftelse.java
+++ src/main/java/no/nav/foreldrepenger/autotest/klienter/fpsak/behandlinger/dto/aksjonspunktbekreftelse/papirsoknad/PapirSoknadEngangstonadBekreftelse.java
@@ -3,12 +3,43 @@ package no.nav.foreldrepenger.autotest.klienter.fpsak.behandlinger.dto.aksjonspu
 import no.nav.foreldrepenger.autotest.klienter.fpsak.behandlinger.dto.aksjonspunktbekreftelse.AksjonspunktBekreftelse;
 import no.nav.foreldrepenger.autotest.klienter.fpsak.behandlinger.dto.aksjonspunktbekreftelse.BekreftelseKode;
 import no.nav.foreldrepenger.autotest.klienter.fpsak.behandlinger.dto.behandling.Behandling;
+import no.nav.foreldrepenger.autotest.klienter.fpsak.behandlinger.dto.behandling.papirsøknad.AnnenForelderDto;
 import no.nav.foreldrepenger.autotest.klienter.fpsak.fagsak.dto.Fagsak;
+import no.nav.foreldrepenger.vtp.dokumentgenerator.foreldrepengesoknad.SøkersRolle;
+
+import java.time.LocalDate;
+import java.util.Collections;
+import java.util.List;
 
 @BekreftelseKode(kode="5012")
 public class PapirSoknadEngangstonadBekreftelse extends AksjonspunktBekreftelse {
+    protected String tema = "FODSL";
+    protected String soknadstype = "ES";
+    protected LocalDate mottattDato = LocalDate.now().minusDays(10);
+
+    protected boolean harFremtidigOppholdUtlands = false;
+    protected boolean harTidligereOppholdUtenlands = false;
+    protected boolean oppholdINorge = true;
+
+    protected String soker = "MOR";
+    protected boolean erBarnetFodt = true;
+    protected Integer antallBarn = 1;
+    protected List<LocalDate> foedselsDato =  Collections.singletonList(LocalDate.now().minusWeeks(1));
+    protected AnnenForelderDto annenForelder = new AnnenForelderDto();
+
+
+
 
     public PapirSoknadEngangstonadBekreftelse(Fagsak fagsak, Behandling behandling) {
         super(fagsak, behandling);
     }
+    public PapirSoknadEngangstonadBekreftelse setSøker (SøkersRolle søker) {
+        this.soker = søker.name();
+        return this;
+    }
+    public PapirSoknadEngangstonadBekreftelse setFoedselsDato(LocalDate foedselsDato) {
+        this.foedselsDato = Collections.singletonList(foedselsDato);
+        return this;
+    }
+
 }
