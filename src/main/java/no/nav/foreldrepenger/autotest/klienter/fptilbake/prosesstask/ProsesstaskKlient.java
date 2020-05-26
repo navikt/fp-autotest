bbc6b90d706338ab21422d1ee8c4942101a00d2b
Joakim Schweizer <45486768+S150405@users.noreply.github.com>
2020-05-26 08:52:32 +0200

    Lagt til venting på prosess_task metode mot fptilbake klienten. Benytter dto fra fpsak. (#253)

---
+++ src/main/java/no/nav/foreldrepenger/autotest/klienter/fptilbake/prosesstask/ProsesstaskKlient.java
@@ -0,0 +1,33 @@
+package no.nav.foreldrepenger.autotest.klienter.fptilbake.prosesstask;
+
+import no.nav.foreldrepenger.autotest.klienter.fpsak.prosesstask.dto.ProsessTaskListItemDto;
+import no.nav.foreldrepenger.autotest.klienter.fpsak.prosesstask.dto.ProsesstaskDto;
+import no.nav.foreldrepenger.autotest.klienter.fpsak.prosesstask.dto.ProsesstaskResultatDto;
+import no.nav.foreldrepenger.autotest.klienter.fpsak.prosesstask.dto.SokeFilterDto;
+import no.nav.foreldrepenger.autotest.klienter.fptilbake.FptilbakeKlient;
+import no.nav.foreldrepenger.autotest.util.http.HttpSession;
+import no.nav.foreldrepenger.autotest.util.http.rest.StatusRange;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ProsesstaskKlient extends FptilbakeKlient {
+
+    private static String PROSESSTASK_URL = "/prosesstask";
+    private static String PROSESSTASK_LIST_URL = PROSESSTASK_URL + "/list";
+    private static String PROSESSTASK_LAUNCH_URL = PROSESSTASK_URL + "/launch";
+
+    public ProsesstaskKlient(HttpSession session) {
+        super(session);
+    }
+
+    public List<ProsessTaskListItemDto> list(SokeFilterDto sokeFilter) {
+        String url = hentRestRotUrl() + PROSESSTASK_LIST_URL;
+        return postOgHentJson(url, sokeFilter, hentObjectMapper().getTypeFactory().constructCollectionType(ArrayList.class, ProsessTaskListItemDto.class), StatusRange.STATUS_SUCCESS);
+    }
+
+    public ProsesstaskResultatDto launch(ProsesstaskDto prosessTask) {
+        String url = hentRestRotUrl() + PROSESSTASK_LAUNCH_URL;
+        return postOgHentJson(url, prosessTask, ProsesstaskResultatDto.class, StatusRange.STATUS_SUCCESS);
+    }
+}
