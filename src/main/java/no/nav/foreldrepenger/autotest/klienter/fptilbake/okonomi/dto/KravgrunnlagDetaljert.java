4a2d47c3218434e7ee22b86987bb1bb57326355d
Saikat Halder <saikat.halder@nav.no>
2020-06-25 16:02:17 +0200

    PFP-9416 endret kravgrunnlag periode til enkel dag for engangsstønad

--- src/main/java/no/nav/foreldrepenger/autotest/klienter/fptilbake/okonomi/dto/KravgrunnlagDetaljert.java
+++ src/main/java/no/nav/foreldrepenger/autotest/klienter/fptilbake/okonomi/dto/KravgrunnlagDetaljert.java
@@ -2,6 +2,7 @@ package no.nav.foreldrepenger.autotest.klienter.fptilbake.okonomi.dto;
 
 import java.math.BigDecimal;
 import java.text.SimpleDateFormat;
+import java.time.DayOfWeek;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.Date;
@@ -73,8 +74,8 @@ public class KravgrunnlagDetaljert {
             throw new IllegalStateException("Periode for Engangsstønad ikke tillatt for fagområde: "+this.fagOmrådeKode);
         }
         KravgrunnlagPeriode kravgrunnlagPeriode = new KravgrunnlagPeriode(
-                LocalDate.now().minusMonths(6).withDayOfMonth(1).toString(),
-                LocalDate.now().minusMonths(6).withDayOfMonth(LocalDate.now().minusMonths(6).lengthOfMonth()).toString(),
+                LocalDate.now().minusMonths(6).with(DayOfWeek.MONDAY).toString(),
+                LocalDate.now().minusMonths(6).with(DayOfWeek.MONDAY).toString(),
                 BigDecimal.ZERO);
         kravgrunnlagPeriode.leggTilPosteringForEngangsstonad();
         this.perioder.add(kravgrunnlagPeriode);
