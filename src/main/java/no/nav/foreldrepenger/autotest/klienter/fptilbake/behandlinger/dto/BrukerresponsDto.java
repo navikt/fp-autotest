b885e1535b578028f623f88b3702267a5fd96a55
Joakim Schweizer <joakim.schweizer@nav.no>
2020-07-09 13:58:09 +0200

    Fikset review kommentar

--- src/main/java/no/nav/foreldrepenger/autotest/klienter/fptilbake/behandlinger/dto/BrukerresponsDto.java
+++ src/main/java/no/nav/foreldrepenger/autotest/klienter/fptilbake/behandlinger/dto/BrukerresponsDto.java
@@ -1,12 +1,12 @@
 package no.nav.foreldrepenger.autotest.klienter.fptilbake.behandlinger.dto;
 
 public class BrukerresponsDto {
-    protected int behandlingId;
+    protected Long behandlingId;
     protected boolean akseptertFaktagrunnlag;
     protected String kildeKanal;
 
     public BrukerresponsDto(int behandlingId) {
-        this.behandlingId = behandlingId;
+        this.behandlingId = (long) behandlingId;
         this.kildeKanal = "MANUELL";
     }
 
