d72965e2e7781001b173e0a7c6c368bb0431f492
Espen Waaga <espen.waaga@nav.no>
2020-08-19 09:44:26 +0200

    Legger til eksternReferanseId/kanalreferanse for JournalpostMottak.

--- src/main/java/no/nav/foreldrepenger/autotest/klienter/fpsak/fordel/dto/JournalpostMottak.java
+++ src/main/java/no/nav/foreldrepenger/autotest/klienter/fpsak/fordel/dto/JournalpostMottak.java
@@ -6,10 +6,12 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class JournalpostMottak {
+    private static int inkrementForEksternReferanse = 0;
 
     protected String saksnummer;
     protected String journalpostId;
     protected String forsendelseId;
+    protected String eksternReferanseId;
     protected String behandlingstemaOffisiellKode;
     protected String dokumentTypeIdOffisiellKode;
     protected String forsendelseMottatt;
@@ -29,6 +31,7 @@ public class JournalpostMottak {
         this.journalpostId = journalpostId;
         this.forsendelseMottatt = forsendelseMottatt;
         this.behandlingstemaOffisiellKode = behandlingstemaOffisiellKode;
+        this.eksternReferanseId = lagUnikEksternReferanseId();
     }
 
     public String getDokumentTypeIdOffisiellKode() {
@@ -62,4 +65,16 @@ public class JournalpostMottak {
     public void setDokumentKategoriOffisiellKode(String dokumentKategoriOffisiellKode) {
         this.dokumentKategoriOffisiellKode = dokumentKategoriOffisiellKode;
     }
+
+    private String lagUnikEksternReferanseId() {
+        String signifikanteSifre = String.valueOf(inkrementForEksternReferanse);
+        StringBuilder sb = new StringBuilder();
+        while(sb.length() + signifikanteSifre.length() < 8) {
+            sb.append('0');
+        }
+        sb.append(signifikanteSifre);
+        inkrementForEksternReferanse++;
+        return "AR" + sb.toString();
+    }
+
 }
