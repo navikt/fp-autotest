4f60d79ef6776d532c4ec5152aabc5a765e244e5
Joakim Schweizer <joakim.schweizer@nav.no>
2020-08-28 14:47:10 +0200

    Sletter linjer istedenfor å kommentere ut...

--- src/test/java/no/nav/foreldrepenger/autotest/fptilbake/engangsstonad/Tilbakekreving.java
+++ src/test/java/no/nav/foreldrepenger/autotest/fptilbake/engangsstonad/Tilbakekreving.java
@@ -139,11 +139,6 @@ public class Tilbakekreving extends FptilbakeTestBase {
                 .setVerge(testscenario.getPersonopplysninger().getAnnenpartIdent());
         saksbehandler.bekreftAksjonspunkt(avklarFaktaVergeBekreftelse);
 
-//        VurderManglendeFodselBekreftelse vurderManglendeFodselBekreftelse = saksbehandler
-//                .hentAksjonspunktbekreftelse(VurderManglendeFodselBekreftelse.class);
-//        vurderManglendeFodselBekreftelse.bekreftDokumentasjonForeligger(1, LocalDate.now().minusMonths(1));
-//        saksbehandler.bekreftAksjonspunkt(vurderManglendeFodselBekreftelse);
-
         AvklarBrukerHarGyldigPeriodeBekreftelse avklarBrukerHarGyldigPeriodeBekreftelse = saksbehandler
                 .hentAksjonspunktbekreftelse(AvklarBrukerHarGyldigPeriodeBekreftelse.class);
         avklarBrukerHarGyldigPeriodeBekreftelse.setVurdering(
@@ -152,12 +147,11 @@ public class Tilbakekreving extends FptilbakeTestBase {
         saksbehandler.bekreftAksjonspunkt(avklarBrukerHarGyldigPeriodeBekreftelse);
         saksbehandler.bekreftAksjonspunktMedDefaultVerdier(ForeslåVedtakBekreftelse.class);
 
-//        beslutter.erLoggetInnMedRolle(Aktoer.Rolle.BESLUTTER);
-//        beslutter.hentFagsak(saksnummer);
-//
-//        FatterVedtakBekreftelse bekreftelse = beslutter.hentAksjonspunktbekreftelse(FatterVedtakBekreftelse.class);
-//        bekreftelse.godkjennAksjonspunkt(saksbehandler.hentAksjonspunkt(AksjonspunktKoder.SJEKK_MANGLENDE_FØDSEL));
-//        beslutter.fattVedtakOgVentTilAvsluttetBehandling(bekreftelse);
+        beslutter.erLoggetInnMedRolle(Aktoer.Rolle.BESLUTTER);
+        beslutter.hentFagsak(saksnummer);
+
+        FatterVedtakBekreftelse bekreftelse = beslutter.hentAksjonspunktbekreftelse(FatterVedtakBekreftelse.class);
+        beslutter.fattVedtakOgVentTilAvsluttetBehandling(bekreftelse);
 
         tbksaksbehandler.erLoggetInnMedRolle(Aktoer.Rolle.SAKSBEHANDLER);
         tbksaksbehandler.opprettTilbakekreving(saksnummer, saksbehandler.valgtBehandling.uuid, ytelseType);
