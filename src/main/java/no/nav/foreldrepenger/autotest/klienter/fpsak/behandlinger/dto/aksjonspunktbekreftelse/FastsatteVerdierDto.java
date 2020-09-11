b3f71a338a91d69511fc641604f21867c8d15cca
Espen Velsvik <espenjv@users.noreply.github.com>
2020-09-11 14:41:12 +0200

    NOJIRA Rename felt i fordeling (#345)

--- src/main/java/no/nav/foreldrepenger/autotest/klienter/fpsak/behandlinger/dto/aksjonspunktbekreftelse/FastsatteVerdierDto.java
+++ src/main/java/no/nav/foreldrepenger/autotest/klienter/fpsak/behandlinger/dto/aksjonspunktbekreftelse/FastsatteVerdierDto.java
@@ -7,24 +7,17 @@ import no.nav.foreldrepenger.autotest.klienter.fpsak.kodeverk.dto.Kode;
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class FastsatteVerdierDto {
 
-    protected Integer refusjon;
     protected Integer refusjonPrÅr;
-    protected Integer fastsattBeløp;
-    protected Integer fastsattÅrsbeløp;
+    protected Integer fastsattÅrsbeløpInklNaturalytelse;
     protected Kode inntektskategori;
-    protected Boolean skalHaBesteberegning;
-
-    public FastsatteVerdierDto(Integer fastsattBeløp) {
-        this.fastsattBeløp = fastsattBeløp;
-    }
 
     public FastsatteVerdierDto(Integer fastsattÅrsbeløp, Kode inntektskategori) {
-        this.fastsattÅrsbeløp = fastsattÅrsbeløp;
+        this.fastsattÅrsbeløpInklNaturalytelse = fastsattÅrsbeløp;
         this.inntektskategori = inntektskategori;
     }
 
     public FastsatteVerdierDto(Integer fastsattÅrsbeløp, Integer refusjonPrÅr, Kode inntektskategori) {
-        this.fastsattÅrsbeløp = fastsattÅrsbeløp;
+        this.fastsattÅrsbeløpInklNaturalytelse = fastsattÅrsbeløp;
         this.inntektskategori = inntektskategori;
         this.refusjonPrÅr = refusjonPrÅr;
     }
