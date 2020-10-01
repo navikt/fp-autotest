130b58026c05020fc6857ff837bcd08e02d72482
Espen Waaga <espen.waaga@nav.no>
2020-10-01 10:21:46 +0200

    Lokal utvikling: Oppdaterer script for lokal utvikling av fptilbake/fpoppdrag

--- lokal-utvikling/lokal-utvikling-fpoppdrag-og-fptilbake.sh
+++ lokal-utvikling/lokal-utvikling-fpoppdrag-og-fptilbake.sh
@@ -1,14 +1,12 @@
 #!/usr/bin/env bash
 # Dette scriptet setter opp avhengighetene slik at fpoppdrag og fptilbake kan kjøres utenfor Docker Compose i IDE.
-# Denne konfigurasjonen setter ikke opp fprisk, men bruker mock av fprisk i VTP istedenfor.
 
 ARGUMENT=${1}
 
 if [[ $ARGUMENT == down ]]; then
   docker-compose -f docker-compose-lokal/docker-compose.yml down
 else
-  options="--mock fprisk"
-  sh ./setup-lokal-utvikling.sh $options fpoppdrag fptilbake
+  sh ./setup-lokal-utvikling.sh fpoppdrag fptilbake
 
   if [ -f .env ]; then
     echo "Bruker applikasjonsversjonene som er definert i eksisterende .env fil: $(pwd)/.env"
@@ -16,6 +14,6 @@ else
     cp docker-compose-lokal/.env .env
   fi
 
-  docker-compose -f docker-compose-lokal/docker-compose.yml pull
-  docker-compose -f docker-compose-lokal/docker-compose.yml up --detach --scale fptilbake=0 --scale fpoppdrag=0
+  docker-compose -f docker-compose-lokal/docker-compose.yml pull --include-deps fpformidling fprisk fpabonnent
+  docker-compose -f docker-compose-lokal/docker-compose.yml up --detach --scale fptilbake=0 --scale fpoppdrag=0 --scale fpfrontend=0
 fi
