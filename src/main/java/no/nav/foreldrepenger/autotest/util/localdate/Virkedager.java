ac31a7ebc13a8457d9ad978c00221afd01ffebca
Jens-Otto Larsen <jens-otto.larsen@nav.no>
2021-02-07 22:57:06 +0100

    NOJIRA gjør helgesikker - eneste bruk av 570

--- src/main/java/no/nav/foreldrepenger/autotest/util/localdate/Virkedager.java
+++ src/main/java/no/nav/foreldrepenger/autotest/util/localdate/Virkedager.java
@@ -77,4 +77,20 @@ public class Virkedager {
         return dato.getDayOfWeek().equals(DayOfWeek.SATURDAY) || dato.getDayOfWeek().equals(DayOfWeek.SUNDAY);
     }
 
+    public static LocalDate helgejustertTilMandag(LocalDate dato) {
+        return switch (dato.getDayOfWeek()) {
+            case SATURDAY -> dato.plusDays(2);
+            case SUNDAY -> dato.plusDays(1);
+            default -> dato;
+        };
+    }
+
+    public static LocalDate helgejustertTilFredag(LocalDate dato) {
+        return switch (dato.getDayOfWeek()) {
+            case SATURDAY -> dato.minusDays(1);
+            case SUNDAY -> dato.minusDays(2);
+            default -> dato;
+        };
+    }
+
 }
