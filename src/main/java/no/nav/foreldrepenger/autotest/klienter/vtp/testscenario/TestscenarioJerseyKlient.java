1f3c13695612a84e6833b5861635398838c5a0c3
Michal J. Sladek <michal.sladek@nav.no>
2021-03-12 15:15:40 +0100

    LoggTest etter alle testene ble kjørt. Sjekker alle containere etter logginnslag med  exceptions, sensitive strenger, om loggen har innhold.

--- src/main/java/no/nav/foreldrepenger/autotest/klienter/vtp/testscenario/TestscenarioJerseyKlient.java
+++ src/main/java/no/nav/foreldrepenger/autotest/klienter/vtp/testscenario/TestscenarioJerseyKlient.java
@@ -2,9 +2,15 @@ package no.nav.foreldrepenger.autotest.klienter.vtp.testscenario;
 
 import static javax.ws.rs.client.Entity.json;
 
+import java.util.List;
+
+import javax.ws.rs.core.GenericType;
+import javax.ws.rs.core.Response;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import io.qameta.allure.Step;
 import no.nav.foreldrepenger.autotest.klienter.vtp.VTPJerseyKlient;
 import no.nav.foreldrepenger.vtp.kontrakter.TestscenarioDto;
 
@@ -18,6 +24,7 @@ public class TestscenarioJerseyKlient extends VTPJerseyKlient {
         super();
     }
 
+    @Step("Oppretter testscenario")
     public TestscenarioDto opprettTestscenarioMedAktorId(String key, Object testscenarioObject, String aktorId, String ident) {
         var testscenarioDto = client.target(base)
                 .path(TESTSCENARIO_I_AUTOTEST_POST_URL)
@@ -30,6 +37,7 @@ public class TestscenarioJerseyKlient extends VTPJerseyKlient {
         return testscenarioDto;
     }
 
+    @Step("Oppretter testscenario")
     public TestscenarioDto opprettTestscenario(String key, Object testscenarioObject) {
         var testscenarioDto = client.target(base)
                 .path(TESTSCENARIO_I_AUTOTEST_POST_URL)
@@ -37,6 +45,17 @@ public class TestscenarioJerseyKlient extends VTPJerseyKlient {
                 .post(json(testscenarioObject), TestscenarioDto.class);
         logger.info("Testscenario opprettet: [{}] med hovedsøker: [{}]", key,
                 testscenarioDto.personopplysninger().søkerIdent());
+        return testscenarioDto;
+    }
+
+    @Step("Henter alle instansierte testdatascenarier")
+    public List<TestscenarioDto> hentAlleScenarier() {
+        var testscenarioDto = client.target(base)
+                .path(TESTSCENARIO_I_AUTOTEST_POST_URL)
+                .request()
+                .get(Response.class)
+                .readEntity(new GenericType<List<TestscenarioDto>>() {});
+        logger.info("Klarte ikke hente hente liste med testscenarier");
         return testscenarioDto;
     }
 }
