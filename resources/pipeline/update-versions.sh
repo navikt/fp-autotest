d4144cd1142642bba2e452e326ce724072642877
Espen Waaga <espen.waaga@nav.no>
2021-04-27 16:24:13 +0200

    Reverter oracle tilbake til V11.

--- resources/pipeline/update-versions.sh
+++ resources/pipeline/update-versions.sh
@@ -16,7 +16,7 @@ imageVersion () {
 }
 
 echo POSTGRES_IMAGE="postgres:12" > .env
-echo ORACLE_IMAGE="ghcr.io/navikt/oracle-foreldrepenger:11" >> .env
+echo ORACLE_IMAGE="ghcr.io/navikt/oracle-foreldrepenger:18-migrert" >> .env
 echo AUDIT_NAIS_IMAGE="$(imageVersion "ghcr.io/navikt/fp-audit-nais-mock")" >> .env
 echo VTP_IMAGE="$(imageVersion "ghcr.io/navikt/vtp")" >> .env
 echo FPABAKUS_IMAGE="$(imageVersion "ghcr.io/navikt/fp-abakus")" >> .env
