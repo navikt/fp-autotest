09c2fffbe1eba1fd13ee62136c90a4dff1ec158b
Jens-Otto Larsen <46576810+jolarsen@users.noreply.github.com>
2021-11-01 19:42:25 +0100

    Bugfix/takle dto kodeverk (#591)
    * Ny serialisering fra fptilbake
    
    * Revert

--- src/main/java/no/nav/foreldrepenger/autotest/klienter/fptilbake/behandlinger/dto/aksjonspunkt/AksjonspunktDefinisjon.java
+++ src/main/java/no/nav/foreldrepenger/autotest/klienter/fptilbake/behandlinger/dto/aksjonspunkt/AksjonspunktDefinisjon.java
@@ -1,5 +1,8 @@
 package no.nav.foreldrepenger.autotest.klienter.fptilbake.behandlinger.dto.aksjonspunkt;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class AksjonspunktDefinisjon {
     public String kode;
     public String navn;
