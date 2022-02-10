b73befaa1aac53779a0a1555432d8f10140ef228
Petter Ekern <petter.ekern@nav.no>
2022-02-10 14:04:48 +0100

    TFP-4855: Endringer etter QA

--- src/main/java/no/nav/foreldrepenger/autotest/klienter/fpsak/behandlinger/dto/behandling/arbeidInntektsmelding/ManglendeOpplysningerVurderingDto.java
+++ src/main/java/no/nav/foreldrepenger/autotest/klienter/fpsak/behandlinger/dto/behandling/arbeidInntektsmelding/ManglendeOpplysningerVurderingDto.java
@@ -4,22 +4,8 @@ import java.util.UUID;
 
 import no.nav.foreldrepenger.autotest.domain.foreldrepenger.ArbeidsforholdKomplettVurderingType;
 
-public class ManglendeOpplysningerVurderingDto {
-    private UUID behandlingUuid;
-    private ArbeidsforholdKomplettVurderingType vurdering;
-    private String begrunnelse;
-    private String arbeidsgiverIdent;
-    private String internArbeidsforholdRef;
-
-    public ManglendeOpplysningerVurderingDto(UUID behandlingUuid,
-                                             ArbeidsforholdKomplettVurderingType vurdering,
-                                             String begrunnelse,
-                                             String arbeidsgiverIdent,
-                                             String internArbeidsforholdRef) {
-        this.behandlingUuid = behandlingUuid;
-        this.vurdering = vurdering;
-        this.begrunnelse = begrunnelse;
-        this.arbeidsgiverIdent = arbeidsgiverIdent;
-        this.internArbeidsforholdRef = internArbeidsforholdRef;
-    }
-}
+public record ManglendeOpplysningerVurderingDto(UUID behandlingUuid,
+                                                ArbeidsforholdKomplettVurderingType vurdering,
+                                                String begrunnelse,
+                                                String arbeidsgiverIdent,
+                                                String internArbeidsforholdRef){}
--- src/main/java/no/nav/foreldrepenger/autotest/klienter/fpsak/behandlinger/dto/behandling/arbeidInntektsmelding/ManueltArbeidsforholdDto.java
+++ src/main/java/no/nav/foreldrepenger/autotest/klienter/fpsak/behandlinger/dto/behandling/arbeidInntektsmelding/ManueltArbeidsforholdDto.java
@@ -3,39 +3,14 @@ package no.nav.foreldrepenger.autotest.klienter.fpsak.behandlinger.dto.behandlin
 import java.time.LocalDate;
 import java.util.UUID;
 
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-
 import no.nav.foreldrepenger.autotest.domain.foreldrepenger.ArbeidsforholdKomplettVurderingType;
 
-@JsonIgnoreProperties(ignoreUnknown = true)
-public class ManueltArbeidsforholdDto {
-    private UUID behandlingUuid;
-    private String begrunnelse;
-    private String arbeidsgiverIdent;
-    private String internArbeidsforholdRef;
-    private String arbeidsgiverNavn;
-    private LocalDate fom;
-    private LocalDate tom;
-    private Integer stillingsprosent;
-    private ArbeidsforholdKomplettVurderingType vurdering;
-
-    public ManueltArbeidsforholdDto(UUID behandlingUuid,
-                                    String begrunnelse,
-                                    String arbeidsgiverIdent,
-                                    String internArbeidsforholdRef,
-                                    String arbeidsgiverNavn,
-                                    LocalDate fom,
-                                    LocalDate tom,
-                                    Integer stillingsprosent,
-                                    ArbeidsforholdKomplettVurderingType vurdering) {
-        this.behandlingUuid = behandlingUuid;
-        this.begrunnelse = begrunnelse;
-        this.arbeidsgiverIdent = arbeidsgiverIdent;
-        this.internArbeidsforholdRef = internArbeidsforholdRef;
-        this.arbeidsgiverNavn = arbeidsgiverNavn;
-        this.fom = fom;
-        this.tom = tom;
-        this.stillingsprosent = stillingsprosent;
-        this.vurdering = vurdering;
-    }
-}
+public record ManueltArbeidsforholdDto(UUID behandlingUuid,
+                                       String begrunnelse,
+                                       String arbeidsgiverIdent,
+                                       String internArbeidsforholdRef,
+                                       String arbeidsgiverNavn,
+                                       LocalDate fom,
+                                       LocalDate tom,
+                                       Integer stillingsprosent,
+                                       ArbeidsforholdKomplettVurderingType vurdering) {}
