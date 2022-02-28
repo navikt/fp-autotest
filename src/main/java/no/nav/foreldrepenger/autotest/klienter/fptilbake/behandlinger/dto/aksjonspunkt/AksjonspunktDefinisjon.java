10927f2d7d6f584f462034a26a5accb7d0b0c375
Jens-Otto Larsen <jens-otto.larsen@nav.no>
2022-02-28 10:34:09 +0100

    Bedre deserialisering flere kodeverk

--- src/main/java/no/nav/foreldrepenger/autotest/klienter/fptilbake/behandlinger/dto/aksjonspunkt/AksjonspunktDefinisjon.java
+++
@@ -1,9 +0,0 @@
-package no.nav.foreldrepenger.autotest.klienter.fptilbake.behandlinger.dto.aksjonspunkt;
-
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-
-@JsonIgnoreProperties(ignoreUnknown = true)
-public class AksjonspunktDefinisjon {
-    public String kode;
-    public String navn;
-}
--- src/main/java/no/nav/foreldrepenger/autotest/klienter/fptilbake/behandlinger/dto/aksjonspunktbekrefter/ApFaktaFeilutbetalingAarsakHendelseTyper.java
+++
@@ -1,7 +0,0 @@
-package no.nav.foreldrepenger.autotest.klienter.fptilbake.behandlinger.dto.aksjonspunktbekrefter;
-
-public class ApFaktaFeilutbetalingAarsakHendelseTyper {
-    protected String kode;
-    protected String navn;
-    protected String kodeverk;
-}
