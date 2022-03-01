36be10d69092d894d28a98e81ae41bef326f46ef
Jens-Otto Larsen <jens-otto.larsen@nav.no>
2022-03-01 16:16:10 +0100

    Legacy for Inntektskategori

--- src/main/java/no/nav/foreldrepenger/autotest/domain/foreldrepenger/Inntektskategori.java
+++ src/main/java/no/nav/foreldrepenger/autotest/domain/foreldrepenger/Inntektskategori.java
@@ -1,7 +1,15 @@
 package no.nav.foreldrepenger.autotest.domain.foreldrepenger;
 
-import com.fasterxml.jackson.annotation.JsonEnumDefaultValue;
+import java.util.Arrays;
+import java.util.Optional;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import no.nav.foreldrepenger.autotest.util.error.UnexpectedInputException;
+
+@JsonFormat(shape = JsonFormat.Shape.OBJECT)
 public enum Inntektskategori {
 
     ARBEIDSTAKER,
@@ -14,8 +22,32 @@ public enum Inntektskategori {
     JORDBRUKER,
     FISKER,
     ARBEIDSTAKER_UTEN_FERIEPENGER,
-    @JsonEnumDefaultValue
-    UDEFINERT,
+    UDEFINERT("-"),
     ;
 
+    private final String kode;
+
+    Inntektskategori() {
+        this(null);
+    }
+
+    Inntektskategori(String kode) {
+        this.kode = Optional.ofNullable(kode).orElse(name());
+    }
+
+    @JsonCreator(mode = JsonCreator.Mode.DELEGATING)
+    public static Inntektskategori fraKode(@JsonProperty(value = "kode") Object node) {
+        if (node == null) {
+            return null;
+        }
+        var kode = TempAvledeKode.getVerdi(Inntektskategori.class, node, "kode");
+        return Arrays.stream(Inntektskategori.values())
+                .filter(value -> value.getKode().equalsIgnoreCase(kode))
+                .findFirst()
+                .orElseThrow(() -> new UnexpectedInputException("Ikke støttet Inntektskategori " + kode));
+    }
+
+    public String getKode() {
+        return kode;
+    }
 }
