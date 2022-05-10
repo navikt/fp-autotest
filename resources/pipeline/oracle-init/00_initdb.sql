0c5f6f3104429b449865091786e019ddcc2dbe48
Michal J. Sladek <mrsladek@users.noreply.github.com>
2022-05-10 11:28:04 +0200

    Bytter oracle db til 18.4-slim uten baseline (#736)
    * Fjerner loadbalancer FQDN
    * Bytter oracle til 18.4-slim image uten baseline.

--- resources/pipeline/oracle-init/00_initdb.sql
+++ resources/pipeline/oracle-init/00_initdb.sql
@@ -1,7 +1,6 @@
 alter database set TIME_ZONE='Europe/Oslo';
-alter database datafile 1 autoextend on maxsize 1G;
+alter database datafile 1 autoextend on maxsize 2G;
 alter system set recyclebin=OFF DEFERRED;
-alter profile default limit password_life_time unlimited;
 alter system set processes=150 scope=spfile;
 alter system set session_cached_cursors=100 scope=spfile;
 alter system set session_max_open_files=100 scope=spfile;
@@ -9,19 +8,5 @@ alter system set sessions=100 scope=spfile;
 alter system set license_max_sessions=100 scope=spfile;
 alter system set license_sessions_warning=100 scope=spfile;
 alter system set disk_asynch_io=FALSE scope=spfile;
+alter profile default limit password_life_time unlimited;
 alter profile default limit password_verify_function null;
-
-DECLARE userexists INTEGER;
-BEGIN
-  SELECT count(*)
-  INTO userexists
-  FROM SYS.ALL_USERS
-  WHERE USERNAME = 'vl_dba';
-  IF (userexists = 0)
-  THEN
-    EXECUTE IMMEDIATE ('CREATE USER vl_dba IDENTIFIED BY vl_dba');
-  END IF;
-END;
-
-GRANT CREATE USER, CONNECT, RESOURCE, DBA, ALTER SESSION TO vl_dba WITH ADMIN OPTION;
-
