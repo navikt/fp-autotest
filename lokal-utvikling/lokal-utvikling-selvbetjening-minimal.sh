37bdb6bb81427c6e2e6b7b77ef5ff16619521bf9
Espen Waaga <espen.waaga@nav.no>
2022-05-12 15:15:10 +0200

    Legger til støtte for fpinfo-historikk, foreldrepengesoknad-api, foreldrepengesoknad og foreldrepengeoversikt i docker-compose.

---
+++ lokal-utvikling/lokal-utvikling-selvbetjening-minimal.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+ARGUMENT=${1}
+
+
+if [[ $ARGUMENT == down ]]; then
+    docker-compose -f docker-compose-lokal/docker-compose.yml down
+else
+    sh ./setup-lokal-utvikling.sh --mock fptilbake --mock fpoppdrag --mock fpformidling --mock fprisk
+
+    if [ -f .env ]; then
+        echo "Bruker applikasjonsversjonene som er definert i eksisterende .env fil: $(pwd)/.env"
+    else
+        cp docker-compose-lokal/.env .env
+    fi
+    # docker-compose -f docker-compose-lokal/docker-compose.yml pull --include-deps foreldrepengesoknad-api
+    docker-compose -f docker-compose-lokal/docker-compose.yml up --detach foreldrepengesoknad-api
+fi
