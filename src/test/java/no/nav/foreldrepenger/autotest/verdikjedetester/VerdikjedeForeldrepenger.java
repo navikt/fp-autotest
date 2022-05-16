1497c56ba795a063dd97daadf9aaa696b64966e2
Espen Waaga <espen.waaga@nav.no>
2022-05-16 11:52:15 +0200

    IKKE_OPPGITT på BFHR test

--- src/test/java/no/nav/foreldrepenger/autotest/verdikjedetester/VerdikjedeForeldrepenger.java
+++ src/test/java/no/nav/foreldrepenger/autotest/verdikjedetester/VerdikjedeForeldrepenger.java
@@ -23,7 +23,7 @@ import static no.nav.foreldrepenger.autotest.klienter.fpsak.behandlinger.dto.beh
 import static no.nav.foreldrepenger.autotest.klienter.fpsak.behandlinger.dto.behandling.uttak.Saldoer.SaldoVisningStønadskontoType.FORELDREPENGER;
 import static no.nav.foreldrepenger.autotest.klienter.fpsak.behandlinger.dto.behandling.uttak.Saldoer.SaldoVisningStønadskontoType.MINSTERETT;
 import static no.nav.foreldrepenger.common.domain.foreldrepenger.fordeling.MorsAktivitet.ARBEID;
-import static no.nav.foreldrepenger.common.domain.foreldrepenger.fordeling.MorsAktivitet.UFØRE;
+import static no.nav.foreldrepenger.common.domain.foreldrepenger.fordeling.MorsAktivitet.IKKE_OPPGITT;
 import static no.nav.foreldrepenger.common.domain.foreldrepenger.fordeling.MorsAktivitet.UTDANNING;
 import static no.nav.foreldrepenger.common.domain.foreldrepenger.fordeling.StønadskontoType.FEDREKVOTE;
 import static no.nav.foreldrepenger.common.domain.foreldrepenger.fordeling.StønadskontoType.FELLESPERIODE;
@@ -1703,7 +1703,7 @@ class VerdikjedeForeldrepenger extends FpsakTestBase {
         var sisteUttaksperiodeFom = uttakEtterUtsettelseTom.plusDays(1);
         var sisteUttaksperiodeTom = sisteUttaksperiodeFom.plusWeeks(6).minusDays(1);
         var fordeling = generiskFordeling(
-                uttaksperiode(StønadskontoType.FORELDREPENGER, fødselsdato, fødselsdato.plusWeeks(2).minusDays(1), UFØRE), // TODO: Erstatt UFØRE med MINSTRETT eller hva som blir implementert i selvbetjeningsapplikasjonene?
+                uttaksperiode(StønadskontoType.FORELDREPENGER, fødselsdato, fødselsdato.plusWeeks(2).minusDays(1), IKKE_OPPGITT),
                 utsettelsesperiode(UtsettelsesÅrsak.FRI,  utsettelsePeriodeFom, utsettelsePeriodeTom, UTDANNING),
                 uttaksperiode(StønadskontoType.FORELDREPENGER, uttakEtterUtsettelseFom, uttakEtterUtsettelseTom, ARBEID),
                 uttaksperiode(StønadskontoType.FORELDREPENGER, sisteUttaksperiodeFom, sisteUttaksperiodeTom));
