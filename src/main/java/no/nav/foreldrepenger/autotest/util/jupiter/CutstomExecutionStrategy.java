69ab381787f5aae1dd08cb2acd36ab9f1d6cbfdc
Espen Waaga <36693523+espenwaaga@users.noreply.github.com>
2022-10-31 17:36:17 +0100

    GA: Antall tester som kjører i parallell bestemmes av en miljøvariabe… (#893)

--- src/main/java/no/nav/foreldrepenger/autotest/util/jupiter/CutstomExecutionStrategy.java
+++ src/main/java/no/nav/foreldrepenger/autotest/util/jupiter/CutstomExecutionStrategy.java
@@ -4,15 +4,18 @@ import org.junit.platform.engine.ConfigurationParameters;
 import org.junit.platform.engine.support.hierarchical.ParallelExecutionConfiguration;
 import org.junit.platform.engine.support.hierarchical.ParallelExecutionConfigurationStrategy;
 
+import no.nav.foreldrepenger.konfig.Environment;
+
 /**
  * Midlertidig workaround for https://github.com/SeleniumHQ/selenium/issues/10113
  * -> getMinimumRunnable settes til 0.
  */
 class CutstomExecutionStrategy implements ParallelExecutionConfiguration, ParallelExecutionConfigurationStrategy {
 
+    private static final Environment ENV = Environment.current();
     @Override
     public int getParallelism() {
-        return 16;
+        return Integer.parseInt(ENV.getProperty("juipter.parallelism", "4"));
     }
 
     @Override
@@ -22,12 +25,12 @@ class CutstomExecutionStrategy implements ParallelExecutionConfiguration, Parall
 
     @Override
     public int getMaxPoolSize() {
-        return 16;
+        return Integer.parseInt(ENV.getProperty("juipter.parallelism", "4"));
     }
 
     @Override
     public int getCorePoolSize() {
-        return 16;
+        return Integer.parseInt(ENV.getProperty("juipter.parallelism", "4"));
     }
 
     @Override
