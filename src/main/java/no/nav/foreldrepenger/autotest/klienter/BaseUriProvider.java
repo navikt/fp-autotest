cf0dfaf192fa9d1e38a0d79007876e1ac2a6ef67
Michal J. Sladek <michal.sladek@nav.no>
2022-11-24 10:09:10 +0100

    Fikser host.

--- src/main/java/no/nav/foreldrepenger/autotest/klienter/BaseUriProvider.java
+++ src/main/java/no/nav/foreldrepenger/autotest/klienter/BaseUriProvider.java
@@ -3,7 +3,7 @@ package no.nav.foreldrepenger.autotest.klienter;
 import java.net.URI;
 
 public class BaseUriProvider {
-    public static final URI AZURE_ROOT = URI.create("http://host.docker.internal:8085");
+    public static final URI AZURE_ROOT = URI.create("http://localhost:8085");
     public static final URI VTP_ROOT = URI.create("http://localhost:8060");
     public static final URI VTP_BASE = URI.create("http://127.0.0.1:8060/rest/api");
     public static final URI FPSAK_BASE = URI.create("http://127.0.0.1:8080/fpsak/api");
