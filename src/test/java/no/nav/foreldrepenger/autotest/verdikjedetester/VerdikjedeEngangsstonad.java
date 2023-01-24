8ad0e17231aad8c82ae8d764b609855f36b035fb
Anders Palfi <anderspalfi@gmail.com>
2023-01-24 14:01:36 +0100

    TFP-5384 test for saksmodell engangsstønad

--- src/test/java/no/nav/foreldrepenger/autotest/verdikjedetester/VerdikjedeEngangsstonad.java
+++ src/test/java/no/nav/foreldrepenger/autotest/verdikjedetester/VerdikjedeEngangsstonad.java
@@ -18,6 +18,7 @@ import no.nav.foreldrepenger.autotest.klienter.fpsak.historikk.dto.Historikkinns
 import no.nav.foreldrepenger.autotest.util.pdf.Pdf;
 import no.nav.foreldrepenger.autotest.util.testscenario.modell.Familie;
 import no.nav.foreldrepenger.common.domain.BrukerRolle;
+import no.nav.foreldrepenger.common.innsyn.v2.BehandlingTilstand;
 
 @Tag("verdikjede")
 class VerdikjedeEngangsstonad extends VerdikjedeTestBase {
@@ -59,5 +60,53 @@ class VerdikjedeEngangsstonad extends VerdikjedeTestBase {
         assertThat(Pdf.is_pdf(pdf))
                 .as("Sjekker om byte array er av typen PDF")
                 .isTrue();
+    }
+
+    @Test
+    @DisplayName("2: Verifiser innsyn har korrekt data")
+    @Description("Verifiserer at innsyn har korrekt data og sammenligner med vedtaket med det saksbehandlerene ser")
+    void mor_innsyn_verifsere() {
+        var familie = new Familie("505");
+        var termindato = LocalDate.now().plusWeeks(3);
+        var søknad = SøknadEngangsstønadErketyper.lagEngangstønadTermin(BrukerRolle.MOR, termindato);
+        var mor = familie.mor();
+        var saksnummer = mor.søk(søknad.build());
+
+        saksbehandler.hentFagsak(saksnummer);
+        var avklarFaktaTerminBekreftelse = saksbehandler
+                .hentAksjonspunktbekreftelse(AvklarFaktaTerminBekreftelse.class);
+
+        var esSaker = mor.innsyn().hentSaker().engangsstønad();
+        assertThat(esSaker).hasSize(1);
+        var esSak = esSaker.stream().findFirst().orElseThrow();
+        assertThat(esSak.saksnummer().value()).isEqualTo(saksnummer.value());
+        assertThat(esSak.sakAvsluttet()).isFalse();
+        assertThat(esSak.gjelderAdopsjon()).isFalse();
+        assertThat(esSak.åpenBehandling().tilstand()).isEqualTo(BehandlingTilstand.UNDER_BEHANDLING);
+        assertThat(esSak.familiehendelse().termindato()).isEqualTo(termindato);
+        assertThat(esSak.familiehendelse().fødselsdato()).isNull();
+        assertThat(esSak.familiehendelse().antallBarn()).isEqualTo(1);
+        assertThat(esSak.familiehendelse().omsorgsovertakelse()).isNull();
+
+        avklarFaktaTerminBekreftelse.setBegrunnelse("Informasjon er hentet fra søknadden og godkjennes av autotest.");
+        saksbehandler.bekreftAksjonspunkt(avklarFaktaTerminBekreftelse);
+        var avklarLovligOppholdBekreftelse = saksbehandler
+                .hentAksjonspunktbekreftelse(AvklarLovligOppholdBekreftelse.class);
+        avklarLovligOppholdBekreftelse.bekreftBrukerHarLovligOpphold();
+        saksbehandler.bekreftAksjonspunkt(avklarLovligOppholdBekreftelse);
+        foreslårOgFatterVedtakVenterTilAvsluttetBehandlingOgSjekkerOmBrevErSendt(saksnummer, false);
+
+        var esSakerEtterVedtak = mor.innsyn().hentSaker().engangsstønad();
+        assertThat(esSakerEtterVedtak).hasSize(1);
+        var esSakEtterVedtak = esSakerEtterVedtak.stream().findFirst().orElseThrow();
+        assertThat(esSakEtterVedtak.saksnummer().value()).isEqualTo(saksnummer.value());
+        assertThat(esSakEtterVedtak.sakAvsluttet()).isTrue();
+        assertThat(esSakEtterVedtak.åpenBehandling()).isNull();
+        assertThat(esSakEtterVedtak.gjelderAdopsjon()).isFalse();
+        assertThat(esSakEtterVedtak.familiehendelse().termindato()).isEqualTo(termindato);
+        assertThat(esSakEtterVedtak.familiehendelse().fødselsdato()).isNull();
+        assertThat(esSakEtterVedtak.familiehendelse().antallBarn()).isEqualTo(1);
+        assertThat(esSakEtterVedtak.familiehendelse().omsorgsovertakelse()).isNull();
+
     }
 }
