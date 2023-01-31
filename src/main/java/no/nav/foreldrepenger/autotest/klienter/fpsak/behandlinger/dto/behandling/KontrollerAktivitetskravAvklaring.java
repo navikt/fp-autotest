b6129780f6302669a001f19326749be550068447
Anders Palfi <anderspalfi@gmail.com>
2023-01-31 15:21:53 +0100

    Sletter ubrukte klasser, gamle fakta uttak

--- src/main/java/no/nav/foreldrepenger/autotest/klienter/fpsak/behandlinger/dto/behandling/KontrollerAktivitetskravAvklaring.java
+++
@@ -1,9 +0,0 @@
-package no.nav.foreldrepenger.autotest.klienter.fpsak.behandlinger.dto.behandling;
-
-public enum KontrollerAktivitetskravAvklaring  {
-
-    I_AKTIVITET,
-    IKKE_I_AKTIVITET_IKKE_DOKUMENTERT,
-    IKKE_I_AKTIVITET_DOKUMENTERT;
-
-}
--- src/main/java/no/nav/foreldrepenger/autotest/klienter/fpsak/behandlinger/dto/behandling/KontrollerFaktaData.java
+++
@@ -1,7 +0,0 @@
-package no.nav.foreldrepenger.autotest.klienter.fpsak.behandlinger.dto.behandling;
-
-import java.util.List;
-
-public record KontrollerFaktaData(List<KontrollerFaktaPeriode> perioder) {
-
-}
