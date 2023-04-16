ce85daa53bcfb763a40047aea77d0a1a65e264b7
Jens-Otto Larsen <jens-otto.larsen@nav.no>
2023-04-16 18:21:58 +0200

    Revert "Tilbyr ny workflow for kjøring av tester (#1065)"
    This reverts commit db7c6f2b080bea668eb43efe3b4c3fa234b51af6.

--- resources/allure/set-environment-variables.sh
+++ resources/allure/set-environment-variables.sh
@@ -10,8 +10,4 @@ else
   echo trigger="$TRIGGER" >>environment.properties
 fi
 
-if [ $1 == 'fpsak' ]; then
-    grep -e '^disclaimer\|^trigger\|^AUDIT_NAIS_IMAGE\|^ORACLE_IMAGE\|^POSTGRES_IMAGE\|^VTP_IMAGE\|^FPABAKUS_IMAGE\|^FPSAK_IMAGE' ../../lokal-utvikling/docker-compose-lokal/.env >> environment.properties
-else
-    cat ../../lokal-utvikling/docker-compose-lokal/.env >> environment.properties
-fi
+cat ../pipeline/.env >> environment.properties
