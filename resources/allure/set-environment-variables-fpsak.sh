3ce7c4d65369b839b9e75ff5f1d489b78acb539d
Jens-Otto Larsen <46576810+jolarsen@users.noreply.github.com>
2023-04-16 18:33:31 +0200

    Revert failing autotest (#1066)
    * Revert "Fikser set-environment-variables.sh som brukes ved generering av testrapport"
    
    This reverts commit 9055c970f1c2fe04d87784ada5e879a3f8552a3a.
    
    * Revert "Tilbyr ny workflow for kjøring av tester (#1065)"
    
    This reverts commit db7c6f2b080bea668eb43efe3b4c3fa234b51af6.

---
+++ resources/allure/set-environment-variables-fpsak.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+cd $(dirname $0)
+
+TRIGGER=${1}
+
+if [ -z ${1+x} ]; then
+  echo trigger="navikt/fp-autotest" >>environment.properties
+else
+  echo trigger="$TRIGGER" >>environment.properties
+fi
+
+grep -e '^disclaimer\|^trigger\|^AUDIT_NAIS_IMAGE\|^ORACLE_IMAGE\|^POSTGRES_IMAGE\|^VTP_IMAGE\|^FPABAKUS_IMAGE\|^FPSAK_IMAGE' ../../lokal-utvikling/docker-compose-lokal/.env >> environment.properties
