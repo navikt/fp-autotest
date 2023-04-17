777ce9fc91b34b2787b0c8a16f216db2b2516e09
espenwaaga <espen.waaga@nav.no>
2023-04-17 09:17:59 +0200

    Fikser set-environment-variables.sh for flere test suites

--- resources/allure/set-environment-variables.sh
+++ resources/allure/set-environment-variables.sh
@@ -10,7 +10,7 @@ else
   echo trigger="$TRIGGER" >>environment.properties
 fi
 
-if [ $1 == 'fpsak' ]; then
+if [ $2 == 'fpsak' ]; then
     grep -e '^disclaimer\|^trigger\|^AUDIT_NAIS_IMAGE\|^ORACLE_IMAGE\|^POSTGRES_IMAGE\|^VTP_IMAGE\|^FPABAKUS_IMAGE\|^FPSAK_IMAGE' ../../lokal-utvikling/docker-compose-lokal/.env >> environment.properties
 else
     cat ../pipeline/.env >> environment.properties
