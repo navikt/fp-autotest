439a23b0506eae3df731ff37c7ecccc6392a9f76
Michal J. Sladek <michal.sladek@nav.no>
2024-03-08 13:03:34 +0100

    Fix kodeverk

--- src/main/java/no/nav/foreldrepenger/autotest/klienter/fpsak/behandlinger/dto/aksjonspunktbekreftelse/VidereBehandling.java
+++ src/main/java/no/nav/foreldrepenger/autotest/klienter/fpsak/behandlinger/dto/aksjonspunktbekreftelse/VidereBehandling.java
@@ -1,5 +1,5 @@
 package no.nav.foreldrepenger.autotest.klienter.fpsak.behandlinger.dto.aksjonspunktbekreftelse;
 
 public enum VidereBehandling {
-    TILBAKEKR_INFOTRYGD, TILBAKEKR_INNTREKK, TILBAKEKR_IGNORER, TILBAKEKR_OPPDATER
+    TILBAKEKR_OPPRETT, TILBAKEKR_INNTREKK, TILBAKEKR_IGNORER, TILBAKEKR_OPPDATER
 }
