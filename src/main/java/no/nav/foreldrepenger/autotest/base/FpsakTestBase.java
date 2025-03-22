1b432c449dc0b330e1a0a64197b1407828a5f2c2
Michal J. Sladek <michal.sladek@nav.no>
2025-03-22 11:51:36 +0100

    Legger inn BrevAssertionBuilder og så tester noen brev på engangsstønad og foreldrepenger

--- src/main/java/no/nav/foreldrepenger/autotest/base/FpsakTestBase.java
+++ src/main/java/no/nav/foreldrepenger/autotest/base/FpsakTestBase.java
@@ -1,12 +1,11 @@
 package no.nav.foreldrepenger.autotest.base;
 
-import no.nav.foreldrepenger.autotest.aktoerer.saksbehandler.fpsak.Oppgavestyrer;
+import static org.assertj.core.api.Assertions.assertThat;
 
-import no.nav.foreldrepenger.autotest.klienter.fpsak.historikk.dto.DokumentTag;
-import no.nav.foreldrepenger.autotest.klienter.fpsak.historikk.dto.HistorikkType;
-import no.nav.foreldrepenger.autotest.util.pdf.Pdf;
-
-import no.nav.foreldrepenger.common.domain.Fødselsnummer;
+import java.io.IOException;
+import java.text.DecimalFormat;
+import java.text.DecimalFormatSymbols;
+import java.util.Set;
 
 import org.apache.pdfbox.Loader;
 import org.apache.pdfbox.text.PDFTextStripper;
@@ -16,20 +15,17 @@ import org.junit.jupiter.api.BeforeEach;
 
 import no.nav.foreldrepenger.autotest.aktoerer.saksbehandler.fpsak.Beslutter;
 import no.nav.foreldrepenger.autotest.aktoerer.saksbehandler.fpsak.Klagebehandler;
+import no.nav.foreldrepenger.autotest.aktoerer.saksbehandler.fpsak.Oppgavestyrer;
 import no.nav.foreldrepenger.autotest.aktoerer.saksbehandler.fpsak.Overstyrer;
 import no.nav.foreldrepenger.autotest.aktoerer.saksbehandler.fpsak.Saksbehandler;
+import no.nav.foreldrepenger.autotest.klienter.fpsak.historikk.dto.DokumentTag;
+import no.nav.foreldrepenger.autotest.klienter.fpsak.historikk.dto.HistorikkType;
 import no.nav.foreldrepenger.autotest.util.log.LoggFormater;
-
-import java.io.IOException;
-import java.util.HashMap;
-import java.util.Map;
-
-import static org.assertj.core.api.Assertions.assertThat;
+import no.nav.foreldrepenger.autotest.util.pdf.Pdf;
+import no.nav.foreldrepenger.common.domain.Fødselsnummer;
 
 // TODO: Fiks opp i testbasene
 public abstract class FpsakTestBase {
-
-    protected static final String OVERSKRIFT_ENGANGSSTØNAD_INNVILGET = "Nav har innvilget søknaden din om engangsstønad";
 
     /*
      * Aktører
@@ -52,36 +48,99 @@ public abstract class FpsakTestBase {
         LoggFormater.leggTilKjørendeTestCaseILogger();
     }
 
-    protected void hentBrevOgSjekkAtInnholdetErRiktig(Map<String, String> ekstraTekstAssertions, Fødselsnummer fnr, DokumentTag dokumentTag) {
-        var dokumentId = saksbehandler.hentHistorikkinnslagAvType(HistorikkType.BREV_SENDT, dokumentTag).dokumenter().getFirst().dokumentId();
+    protected void hentBrevOgSjekkAtInnholdetErRiktig(BrevAssertionBuilder assertionBuilder,
+                                                      Fødselsnummer fnr,
+                                                      DokumentTag dokumentTag) {
+        var dokumentId = saksbehandler.hentHistorikkinnslagAvType(HistorikkType.BREV_SENDT, dokumentTag)
+                .dokumenter()
+                .getFirst()
+                .dokumentId();
         var pdf = saksbehandler.hentJournalførtDokument(dokumentId, "ARKIV");
         assertThat(Pdf.is_pdf(pdf)).as("Sjekker om byte array er av typen PDF").isTrue();
 
-        var brevAssertions = new HashMap<String, String>();
-        brevAssertions.put("Sjekk om riktig fødselsnummer i brevet.", fnr.value().substring(0, 6) + " " + fnr.value().substring(6));
-        brevAssertions.put("Sjekk om riktig hilsen er brukt.", "Nav Familie- og pensjonsytelser");
-        brevAssertions.put("Sjekk rett til klage.", "Du har rett til å klage");
-        brevAssertions.put("Sjekk rett til innsyn.", "Du har rett til innsyn");
-        brevAssertions.put("Sjekk om spørsmål paragraf finnes.", "Har du spørsmål?");
+        assertionBuilder
+                .medEgenndefinertAssertion(formatFnr(fnr))
+                .medKapittelDuHarRettTilInnsyn()
+                .medKapittelHarDuSpørsmål()
+                .medVennligHilsen()
+                .medUnderksriftNFP();
 
-        brevAssertions.putAll(ekstraTekstAssertions);
-
-        sjekkAtBrevetInneholderTekst(pdf, brevAssertions);
+        sjekkAtBrevetInneholderTekst(pdf, assertionBuilder.build());
     }
 
-    protected static void sjekkAtBrevetInneholderTekst(byte[] pdfBytes, Map<String, String> asserts) {
+    protected static String formatFnr(Fødselsnummer fnr) {
+        return fnr.value().substring(0, 6) + " " + fnr.value().substring(6);
+    }
+
+    private static void sjekkAtBrevetInneholderTekst(byte[] pdfBytes, Set<String> asserts) {
         try (var document = Loader.loadPDF(pdfBytes)) {
-            var text = new PDFTextStripper().getText(document);
+            // Fjerne linjeskift for å kunne søke på tvers av linjer
+            var pdfTekst = new PDFTextStripper().getText(document).replace("\n", "");
             var softAssertions = new SoftAssertions();
-            asserts.forEach((assertionTekst, tekst) -> softAssertions.assertThat(text.contains(tekst)).as(assertionTekst).isTrue());
+            asserts.forEach(skalFinnes -> softAssertions.assertThat(pdfTekst.contains(skalFinnes)).as("'%s' finnes ikke".formatted(skalFinnes)).isTrue());
             softAssertions.assertAll();
         } catch (IOException e) {
             Fail.fail("Kunne ikke lese PDFen", e);
         }
     }
 
-    protected static Map<String, String> engangsstønadStandardBrevAssertions() {
-        return Map.of("Sjekk om riktig paragraf er brukt.", "Vedtaket er gjort etter folketrygdloven § 14-17.");
+    protected static String formatBeløp(int beløp) {
+        var symbols = new DecimalFormatSymbols();
+        symbols.setGroupingSeparator(' '); // Explicitly setting a normal space (U+0020)
+        return new DecimalFormat("#,###", symbols).format(beløp);
+    }
+
+    protected static BrevAssertionBuilder engangsstønadInnvilgetAssertionsBuilder() {
+        return BrevAssertionBuilder.ny()
+                .medOverskriftOmInnvilgetEnagangsstønad()
+                .medKapittelDuHarRettTilKlage()
+                .medTekstOmVedtaketEtterFolketrygdloven()
+                .medParagraf_14_17();
+    }
+
+    protected static BrevAssertionBuilder foreldrepengerInnvilget80ProsentAssertionsBuilder() {
+        return foreldrepengerInnvilgetFellesAssertionBuilder().medOverskriftOmInnvilget80ProsentForeldrepenger();
+    }
+
+    protected static BrevAssertionBuilder foreldrepengerInnvilget100ProsentAssertionsBuilder() {
+        return foreldrepengerInnvilgetFellesAssertionBuilder().medOverskriftOmInnvilget100ProsentForeldrepenger();
+    }
+
+    private static BrevAssertionBuilder foreldrepengerInnvilgetFellesAssertionBuilder() {
+        return BrevAssertionBuilder.ny()
+                .medTekstOmVedtaketEtterFolketrygdloven()
+                .medTekstOmBeregningEtterFolketrygdloven()
+                .medParagraf_14_7()
+                .medTekstOmInntektBruktIBeregningen()
+                .medKapittelDuMåMeldeOmEndringer()
+                .medKapittelDuHarRettTilKlage();
+    }
+
+    protected static BrevAssertionBuilder foreldrepengerInnvilgetEndringAssertionsBuilder() {
+        return BrevAssertionBuilder.ny()
+                .medOverskriftOmInnvilgetEndringAvForeldrepenger()
+                .medKapittelDetteHarViInnvilget()
+                .medKapittelDuMåMeldeOmEndringer()
+                .medKapittelDuHarRettTilKlage()
+                .medTekstOmVedtaketEtterFolketrygdloven();
+    }
+
+    protected static BrevAssertionBuilder foreldrepengerAnnuleringAssertionsBuilder() {
+        return BrevAssertionBuilder.ny()
+                .medOverskriftOmInnvilgetAnnuleringAvForeldrepenger()
+                .medTekstOmDuMåSøkeNyForeldrepengerperiodePå()
+                .medTekstOmSøkeSenestDagenFørNyPeriodeEllerBarnetFyllerTreÅr()
+                .medEgenndefinertAssertion(
+                        "Når det er 4 uker igjen til du skal ta ut foreldrepenger, må arbeidsgiveren din sende inn ny inntektsmelding.")
+                .medEgenndefinertAssertion("Du har valgt å ikke ta ut din tidligere innvilgede periode med foreldrepenger")
+                .medTekstOmVedtaketEtterFolketrygdloven()
+                .medParagraf_14_6()
+                .medParagraf_14_7()
+                .medParagraf_14_9()
+                .medParagraf_14_10()
+                .medParagraf_14_11()
+                .medParagraf_14_12()
+                .medKapittelDuHarRettTilKlage();
     }
 
 }
