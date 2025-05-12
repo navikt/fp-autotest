5c85922cc6988cef560b8d36daecbfc2b55a56ce
espenwaaga <espen.waaga@nav.no>
2025-05-12 12:06:38 +0200

    Fjerner eksludering av loggmelding i oversikt

--- src/test/java/no/nav/foreldrepenger/autotest/teknisk/logg/LoggTest.java
+++ src/test/java/no/nav/foreldrepenger/autotest/teknisk/logg/LoggTest.java
@@ -58,7 +58,6 @@ class LoggTest {
             "Error while loading kafka-streams-version.properties",
             "Vil automatisk prøve igjen",
             "Unable to acquire JDBC",
-            "Dummy MinSideVarsel-producer sender", // Logges i fpoversikt i under testing av aktivitetskrav
             "Kan ikke etterlyse inntektsmeldinger når ingen innteksmeldinger mangler", // Logges av formidling pga timingproblemer i verdikjede
             "FP-018669:Feil ved kall til Abakus: Kunne ikke hente grunnlag fra abakus", // Logges i fpsak som konsekvens av det nedenfor
             "duplicate key value violates unique constraint \\\"uidx_kobling_1\\\"" // Logges i fpabakus
