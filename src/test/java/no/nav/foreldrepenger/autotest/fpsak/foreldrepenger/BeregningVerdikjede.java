0f981c3a2b8e1ec0eeab81b4d69adbcd6181e640
remhol <193506135+remhol@users.noreply.github.com>
2025-07-04 10:22:51 +0200

    -

--- src/test/java/no/nav/foreldrepenger/autotest/fpsak/foreldrepenger/BeregningVerdikjede.java
+++ src/test/java/no/nav/foreldrepenger/autotest/fpsak/foreldrepenger/BeregningVerdikjede.java
@@ -186,7 +186,6 @@ class BeregningVerdikjede extends FpsakTestBase {
                 .medAnnenForelder(AnnenforelderMaler.norskMedRettighetNorge(familie.far()));
         var saksnummer = mor.søk(søknad.build());
 
-
         var arbeidsgiver = mor.arbeidsgiver();
         var arbeidsgiverIdentifikator = arbeidsgiver.arbeidsgiverIdentifikator();
         var månedsinntekt = mor.månedsinntekt(arbeidsgiverIdentifikator);
