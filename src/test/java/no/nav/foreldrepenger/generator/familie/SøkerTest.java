1b236b685598086fed59b70d308dacec14d13802
johannbm <johannes.moskvil@gmail.com>
2026-09-10 13:17:20 +0200

    slett noen tester

--- src/test/java/no/nav/foreldrepenger/generator/familie/SøkerTest.java
+++ src/test/java/no/nav/foreldrepenger/generator/familie/SøkerTest.java
@@ -4,7 +4,6 @@ import static no.nav.foreldrepenger.generator.soknad.maler.SøknadForeldrepenger
 import static no.nav.foreldrepenger.generator.familie.generator.TestOrganisasjoner.NAV_OSLO;
 import static no.nav.foreldrepenger.generator.familie.generator.TestOrganisasjoner.NAV_STORD;
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.groups.Tuple.tuple;
 
 import java.lang.reflect.Proxy;
 import java.time.LocalDate;
@@ -74,30 +73,6 @@ class SøkerTest {
     }
 
     @Test
-    @DisplayName("Flere registrerte næringer beholdes i rekkefølge i søkerinfo")
-    void flereRegistrerteNæringerLeggesISøkerinfo() {
-        var person = personMedInntekt(InntektGenerator.ny()
-                        .selvstendigNæringsdrivende(350_000)
-                        .registrertNæring(
-                                "974760673",
-                                "VTP GÅRDSDRIFT",
-                                "ENK",
-                                "Enkeltpersonforetak",
-                                "01.110",
-                                "Dyrking av korn")
-                        .build());
-
-        assertThat(søkerinfo(person).selvstendigNæring())
-                .extracting(
-                        SøkerDto.SelvstendigNæring::navn,
-                        næring -> næring.organisasjonsnummer().value(),
-                        SøkerDto.SelvstendigNæring::næringstype)
-                .containsExactly(
-                        tuple("VTP FISKE", "999999999", NæringDto.Virksomhetstype.FISKE),
-                        tuple("VTP GÅRDSDRIFT", "974760673", NæringDto.Virksomhetstype.JORDBRUK_SKOGBRUK));
-    }
-
-    @Test
     @DisplayName("Ordinære arbeidsforhold og frilansoppdrag legges i hver sin søkerinfo-liste")
     void aaregdataFordelesPåArbeidsforholdOgFrilansoppdrag() {
         var person = personMedInntekt(InntektGenerator.ny()
@@ -122,30 +97,6 @@ class SøkerTest {
                         "NAV FAMILIE- OG PENSJONSYTELSER STORD",
                         LocalDate.now().minusMonths(6),
                         LocalDate.now().minusMonths(1));
-    }
-
-    @Test
-    @DisplayName("Mange frilansoppdrag beholdes med perioder og rekkefølge i søkerinfo")
-    void mangeFrilansoppdragLeggesISøkerinfo() {
-        var inntektYtelse = InntektGenerator.ny();
-        var førsteOppdragFom = LocalDate.now().minusDays(320);
-        for (int i = 0; i < 20; i++) {
-            var fom = førsteOppdragFom.plusDays(i * 16L);
-            inntektYtelse.frilans(NAV_STORD, "frilans-" + (i + 1), 25, fom, fom.plusDays(13), 120_000);
-        }
-        var person = personMedInntekt(inntektYtelse.build());
-
-        var frilansoppdrag = søkerinfo(person).frilansoppdrag();
-
-        assertThat(frilansoppdrag).hasSize(20)
-                .allSatisfy(oppdrag -> assertThat(oppdrag.navn())
-                        .isEqualTo("NAV FAMILIE- OG PENSJONSYTELSER STORD"));
-        assertThat(frilansoppdrag.getFirst())
-                .extracting(SøkerDto.Frilansoppdrag::fom, SøkerDto.Frilansoppdrag::tom)
-                .containsExactly(førsteOppdragFom, førsteOppdragFom.plusDays(13));
-        assertThat(frilansoppdrag.getLast())
-                .extracting(SøkerDto.Frilansoppdrag::fom, SøkerDto.Frilansoppdrag::tom)
-                .containsExactly(førsteOppdragFom.plusDays(19 * 16L), førsteOppdragFom.plusDays(19 * 16L + 13));
     }
 
     private static RegistrertNæringsvirksomhetDto virksomhet(String orgnummer, String næringskode) {
--- src/test/java/no/nav/foreldrepenger/generator/familie/generator/InntektGeneratorTest.java
+++ src/test/java/no/nav/foreldrepenger/generator/familie/generator/InntektGeneratorTest.java
@@ -15,13 +15,20 @@ import no.nav.foreldrepenger.vtp.kontrakter.person.v2.RegistrertNæringsvirksomh
 class InntektGeneratorTest {
 
     @Test
-    @DisplayName("Selvstendig næringsdrivende får Sigrun-inntekt og registrert virksomhet fra Brreg")
+    @DisplayName("Selvstendig næringsdrivende får Sigrun-inntekt og registrerte virksomheter fra Brreg")
     void selvstendigNæringsdrivendeOppretterSigrunOgBrreg() {
         var næringsinntekt = 200_000;
         var inneværendeÅr = LocalDate.now().getYear();
 
         var inntektYtelse = InntektGenerator.ny()
                 .selvstendigNæringsdrivende(næringsinntekt)
+                .registrertNæring(
+                        "974760673",
+                        "VTP GÅRDSDRIFT",
+                        "ENK",
+                        "Enkeltpersonforetak",
+                        "01.110",
+                        "Dyrking av korn")
                 .build();
 
         assertThat(inntektYtelse.skatteopplysninger())
@@ -34,39 +41,20 @@ class InntektGeneratorTest {
                         inneværendeÅr - 3,
                         inneværendeÅr - 4,
                         inneværendeÅr - 5);
-        assertThat(inntektYtelse.registrerteNæringsvirksomheter()).containsExactly(
-                new RegistrertNæringsvirksomhetDto(
-                        "999999999",
-                        "VTP FISKE",
-                        "ENK",
-                        "Enkeltpersonforetak",
-                        "03.110",
-                        "Hav- og kystfiske"));
-    }
-
-    @Test
-    @DisplayName("Flere registrerte næringer legges til i stabil rekkefølge")
-    void flereRegistrerteNæringer() {
-        var inntektYtelse = InntektGenerator.ny()
-                .selvstendigNæringsdrivende(200_000)
-                .registrertNæring(
-                        "974760673",
-                        "VTP GÅRDSDRIFT",
-                        "ENK",
-                        "Enkeltpersonforetak",
-                        "01.110",
-                        "Dyrking av korn")
-                .build();
-
         assertThat(inntektYtelse.registrerteNæringsvirksomheter())
                 .extracting(RegistrertNæringsvirksomhetDto::organisasjonsnummer,
                         RegistrertNæringsvirksomhetDto::navn)
                 .containsExactly(
                         tuple("999999999", "VTP FISKE"),
                         tuple("974760673", "VTP GÅRDSDRIFT"));
-        assertThat(inntektYtelse.skatteopplysninger())
-                .hasSize(5)
-                .allSatisfy(inntektsår -> assertThat(inntektsår.beløp()).isEqualTo(200_000));
+        assertThat(inntektYtelse.registrerteNæringsvirksomheter().getFirst())
+                .isEqualTo(new RegistrertNæringsvirksomhetDto(
+                        "999999999",
+                        "VTP FISKE",
+                        "ENK",
+                        "Enkeltpersonforetak",
+                        "03.110",
+                        "Hav- og kystfiske"));
     }
 
 }
